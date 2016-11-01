61f6bbf1440b290d285b293d15d09f1c1141b159
Fuglen <simon0210@hotmail.dk>
2016-11-01 11:19:34 +0100

    Added permissions for the /fly command

--- MyPlugin/src/me/Fuglen/myplugin/MyPluginFlyCommand.java
+++ MyPlugin/src/me/Fuglen/myplugin/MyPluginFlyCommand.java
@@ -23,13 +23,13 @@ public class MyPluginFlyCommand implements CommandExecutor {
 		
         
         if (cmd.getName().equalsIgnoreCase("fly")) {
-        	if (args.length == 0) {
+        	if (args.length == 0 ) {
             	player.sendMessage(ChatColor.YELLOW + "Brug /fly on = du kan flyve");
             	player.sendMessage(ChatColor.YELLOW + "Brug /fly off = du kan ikke flyve");
             	return true;
             }
         	
-        		//Kommando til /fly on sï¿½ man kan slï¿½ det til.
+        		//Kommando til /fly on så man kan flyve.
                 if (args[0].equalsIgnoreCase("on")) {
                     if (args.length == 1) {
     					player.setAllowFlight(true);
@@ -37,44 +37,60 @@ public class MyPluginFlyCommand implements CommandExecutor {
                     	return true;
                 	}
                 
-                    else if (args.length == 2) {
+                    else if (args.length
+    					== 2 && sender.hasPermission("myplugin.fly.others")
+    					|| sender.hasPermission("myplugin.fly.*")
+    					|| sender.hasPermission("myplugin.*")) {
                     	Player target = Bukkit.getPlayer(args[1]);
         				if (target == null) {
         					sender.sendMessage(ChatColor.RED + "Spilleren er ikke online!");
         					return true;
-        				}
+        					}
                     	target.setAllowFlight(true);
                     	target.sendMessage(ChatColor.YELLOW + sender.getName() + ChatColor.GREEN + ", har givet dig tilladelse til at flyve!");
                     	player.sendMessage(ChatColor.YELLOW + target.getName() + ChatColor.GREEN + ", kan nu flyve!");
                     	return true;
+                    	}
+                    
+                    else {
+					sender.sendMessage(ChatColor.DARK_RED + "Du har ikke adgang til denne kommando.");
                     }
+                return true;
                 }
                 
-                //Kommando til /fly off sï¿½ man kan slï¿½ det fra.
+                //Kommando til /fly off så man ikke længere kan flyve.
                 else if (args[0].equalsIgnoreCase("off")) {
-                    if (args.length == 1) {
+                    if (args.length
+        					== 1 && sender.hasPermission("myplugin.fly")
+        					|| sender.hasPermission("myplugin.fly.*")
+        					|| sender.hasPermission("myplugin.*")) {
     					player.setAllowFlight(false);
-    					player.sendMessage(ChatColor.GREEN + "Du kan ikke lÃ¦ngere flyve!");
+    					player.sendMessage(ChatColor.GREEN + "Du kan ikke længere flyve!");
                     	return true;
                 	}
                 
-                    else if (args.length == 2) {
+                    else if (args.length
+        					== 2 && sender.hasPermission("myplugin.fly.others")
+        					|| sender.hasPermission("myplugin.fly.*")
+        					|| sender.hasPermission("myplugin.*")) {
                     	Player target = Bukkit.getPlayer(args[1]);
         				if (target == null) {
         					sender.sendMessage(ChatColor.RED + "Spilleren er ikke online!");
         					return true;
-        				}
+        					}
                     	target.setAllowFlight(false);
                     	target.sendMessage(ChatColor.YELLOW + sender.getName() + ChatColor.GREEN + ", har fjernet din tilladelse til at flyve!");
-                    	player.sendMessage(ChatColor.YELLOW + target.getName() + ChatColor.GREEN + ", kan ikke lÃ¦ngere flyve!");
+                    	player.sendMessage(ChatColor.YELLOW + target.getName() + ChatColor.GREEN + ", kan ikke længere flyve!");
                     	return true;
-                    }
-                }
-                
-                
-                
+                    	}
+                    else {
+					sender.sendMessage(ChatColor.DARK_RED + "Du har ikke adgang til denne kommando.");
+                    } 
                 return true;
-        }
-        return true;
+                }
+                           
+            return true;
+        	}
+    return false;
     }
 }
